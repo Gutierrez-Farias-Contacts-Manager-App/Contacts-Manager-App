76d9ee9e8f34842f4dc16ccccf543305e9155e0a
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-05-03 16:09:37 -0500

    Finished project

--- src/ContactApp.java
+++ src/ContactApp.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class ContactApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean isContinued = true;
-        AsciiBanners asciiBanners = new AsciiBanners();
+        boolean isContinued = true;//used to exit the application
+        AsciiBanners asciiBanners = new AsciiBanners();//banner from AsciiBanners class
         asciiBanners.hello();
         do {
             System.out.println("1. View contacts.\n" +
@@ -13,11 +13,13 @@ public class ContactApp {
                     "3. Search a contact by name.\n" +
                     "4. Delete an existing contact.\n" +
                     "5. Exit.\n" +
-                    "Enter an option (1, 2, 3, 4 or 5):");
-            String userInput = scanner.nextLine();
+                    "Enter an option (1, 2, 3, 4 or 5):");//Application functions
+            String userInput = scanner.nextLine();//Prompts user for response
+//<-------------------------------View Contacts----------------------------------------------------------------->
             if (userInput.equals("1")) {
-                String fileName = "contacts.txt";
+                String fileName = "contacts.txt";//sets txt file to variable
                 asciiBanners.contactListBanner();
+                System.out.println("Name        | Phone      ");
                 try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
                     String line;
                     while ((line = br.readLine()) != null) {
@@ -31,6 +33,7 @@ public class ContactApp {
                     System.err.println("Error reading file: " + e.getMessage());
                     asciiBanners.trollBanner();
                 }
+//<-------------------------------Add Contact----------------------------------------------------------------->
             } else if (userInput.equals("2")) {
                 String fileName = "contacts.txt";
                 System.out.println("Enter New Contact Name: ");
@@ -47,6 +50,7 @@ public class ContactApp {
                     System.err.println("Error writing to file: " + e.getMessage());
                     asciiBanners.trollBanner();
                 }
+//<-------------------------------Search Contacts----------------------------------------------------------------->
             } else if (userInput.equals("3")) {
                 asciiBanners.monopolyBanner();
                 String fileName = "contacts.txt";
@@ -73,6 +77,7 @@ public class ContactApp {
                     System.out.println("Contact not found: " + name);
                     asciiBanners.trollBanner();
                 }
+//<-------------------------------Delete Contacts----------------------------------------------------------------->
             } else if (userInput.equals("4")) {
                 String fileName = "contacts.txt";
                 System.out.println("Contact name to delete.");
@@ -107,9 +112,13 @@ public class ContactApp {
                 } else {
                     System.out.println("Contact not found: " + nameToDelete);
                 }
+//<-------------------------------Exit App----------------------------------------------------------------->
             } else if (userInput.equals("5")) {
                 asciiBanners.endBanner();
                 isContinued = false;
+            }
+            else {
+                System.out.println("Not a Valid response.....\n Try again!");
             }
         } while (isContinued);
     }
